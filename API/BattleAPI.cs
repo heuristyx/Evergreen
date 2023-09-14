3b7861740bca80b6b12960a695beb0e0113351a7
heuristyx <49342044+heuristyx@users.noreply.github.com>
2023-09-14 19:28:10 +0200

    Some more battle hooks

--- API/BattleAPI.cs
+++ API/BattleAPI.cs
@@ -1,6 +1,6 @@
-using Everhood.Battle;
 using System;
 using UnityEngine;
+using Everhood.Battle;
 
 namespace Evergreen;
 
@@ -10,30 +10,58 @@ public static class BattleAPI {
     public int damage { get; set; }
   }
 
+  public class StateLikeEventArgs : EventArgs {
+    public bool state { get; set; }
+  }
+
   // Event handlers
-  public static event EventHandler<DamageEventArgs> OnDamage;
+  public static event EventHandler<DamageEventArgs> OnTakeDamage;
+  public static event EventHandler<DamageEventArgs> OnDealDamage;
+  public static event EventHandler<StateLikeEventArgs> OnJump;
   public static event EventHandler OnBattleUpdate;
 
   internal static void Init() {
     Evergreen.Log.LogInfo($"Loading Evergreen {nameof(BattleAPI)}");
 
     // Add events to hooks
-    On.Everhood.Battle.BattlePlayer.Damage += HookOnDamage;
+    On.Everhood.Battle.BattlePlayer.Damage += HookOnTakeDamage;
+    On.Everhood.Battle.BattleEnemy.Damage += HookOnDealDamage;
+    On.Everhood.Battle.BattlePlayer.SetJumpState += HookOnJump;
     On.Everhood.Battle.BattleEnemyHealthObserver.Init += HookOnBattleStart;
   }
 
   // Hooks
-  internal static void HookOnDamage(On.Everhood.Battle.BattlePlayer.orig_Damage orig, Everhood.Battle.BattlePlayer self, int damage) {
-    if (OnDamage != null) {
+  internal static void HookOnTakeDamage(On.Everhood.Battle.BattlePlayer.orig_Damage orig, BattlePlayer self, int damage) {
+    if (OnTakeDamage != null) {
       var args = new DamageEventArgs { damage = damage };
-      foreach (EventHandler<DamageEventArgs> e in OnDamage.GetInvocationList()) e?.Invoke(self, args);
+      foreach (EventHandler<DamageEventArgs> e in OnTakeDamage.GetInvocationList()) e?.Invoke(self, args);
       damage = args.damage;
     }
 
     orig(self, damage);
   }
 
-  internal static void HookOnBattleStart(On.Everhood.Battle.BattleEnemyHealthObserver.orig_Init orig, Everhood.Battle.BattleEnemyHealthObserver self) {
+  internal static void HookOnDealDamage(On.Everhood.Battle.BattleEnemy.orig_Damage orig, BattleEnemy self, int damage) {
+    if (OnDealDamage != null) {
+      var args = new DamageEventArgs { damage = damage };
+      foreach (EventHandler<DamageEventArgs> e in OnDealDamage.GetInvocationList()) e?.Invoke(self, args);
+      damage = args.damage;
+    }
+
+    orig(self, damage);
+  }
+
+  internal static void HookOnJump(On.Everhood.Battle.BattlePlayer.orig_SetJumpState orig, BattlePlayer self, bool state) {
+    if (OnJump != null && state) {
+      var args = new StateLikeEventArgs { state = state };
+      foreach (EventHandler<StateLikeEventArgs> e in OnJump.GetInvocationList()) e?.Invoke(self, args);
+      state = args.state;
+    }
+
+    orig(self, state);
+  }
+
+  internal static void HookOnBattleStart(On.Everhood.Battle.BattleEnemyHealthObserver.orig_Init orig, BattleEnemyHealthObserver self) {
     
     GameObject evergreenBattleManager = new GameObject("BattleManager", typeof(BattleManager));
 
--- Evergreen.cs
+++ Evergreen.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Logging;
 
 namespace Evergreen;
