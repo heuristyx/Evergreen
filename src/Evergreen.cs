626c90e8f5beeddb312d75fadb298cda0ca21a34
heuristyx <49342044+heuristyx@users.noreply.github.com>
2023-11-06 21:41:18 +0100

    versioning

--- src/Evergreen.cs
+++ src/Evergreen.cs
@@ -9,7 +9,7 @@ public class Evergreen : BaseUnityPlugin
 {
   public const string Guid = "com.heuristyx.plugins.evergreen";
   public const string Name = "Evergreen";
-  public const string Version = "0.1.0";
+  public const string Version = "1.0.0";
 
   public enum Executable
   {
